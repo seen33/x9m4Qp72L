99d0dfbbb1ca5dc19b132bcbfce2864d62a134ca
seen33 <seen.emel@protonmail.com>
2026-07-15 22:39:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,30 @@
-const notes=[
+const space =
+document.querySelector(".space");
 
-"C4",
-"D4",
-"E4",
-"F4",
-"G4",
-"A4",
-"B4",
-"C5",
-"D5",
-"E5",
-"F5"
 
-];
+const planet =
+document.querySelector(".planet");
+
+
+const final =
+document.querySelector(".final");
 
 
 
-const piano=
-document.getElementById("piano");
+planet.onclick=()=>{
 
 
+space.classList.add("active");
 
-let clicks=0;
-
-let started=false;
-
-
-
-
-
-// ساخت کلیدها
-
-
-notes.forEach(note=>{
-
-
-let key=document.createElement("div");
-
-
-key.className="key";
-
-
-key.dataset.note=note;
-
-
-
-key.onclick=()=>{
-
-
-play(note);
-
-
-key.classList.add("active");
 
 
 setTimeout(()=>{
 
-key.classList.remove("active");
 
-},150);
-
+final.classList.add("show");
 
 
-clicks++;
-
-
-
-if(clicks>=6){
-
-birthday();
-
-}
+},5000);
 
 
 
@@ -78,173 +32,43 @@ birthday();
 
 
 
-piano.appendChild(key);
 
 
+// ساخت ستاره های متحرک
 
-});
+for(let i=0;i<150;i++){
 
 
+let star=document.createElement("div");
 
 
+star.style.position="absolute";
 
+star.style.width="2px";
 
-// صدای پیانو
+star.style.height="2px";
 
+star.style.background="white";
 
-const synth =
-new Tone.PolySynth(
-Tone.Synth,
-{
+star.style.borderRadius="50%";
 
-oscillator:{
-type:"fmtriangle"
-},
 
-envelope:{
-
-attack:.01,
-
-decay:.2,
-
-sustain:.3,
-
-release:1
-
-}
-
-}
-
-).toDestination();
-
-
-
-
-
-function play(note){
-
-
-Tone.start();
-
-
-synth.triggerAttackRelease(
-
-note,
-
-"8n"
-
-);
-
-
-}
-
-
-
-
-
-
-
-// آهنگ تولد
-
-
-const song=[
-
-"G4","G4","A4","G4","C5","B4",
-
-"G4","G4","A4","G4","D5","C5",
-
-"G4","G4","G5","E5","C5","B4","A4",
-
-"F5","F5","E5","C5","D5","C5"
-
-];
-
-
-
-
-
-function birthday(){
-
-
-if(started)return;
-
-
-started=true;
-
-
-
-document
-.getElementById("intro")
-.style.opacity=0;
-
-
-
-song.forEach((n,i)=>{
-
-
-setTimeout(()=>{
-
-
-play(n);
-
-
-},
-
-i*450);
-
-
-
-});
-
-
-
-
-setTimeout(()=>{
-
-
-document
-.getElementById("final")
-.classList
-.add("show");
-
-
-
-},
-
-song.length*450+1000);
-
-
-
-}
-
-
-
-
-
-
-// ذرات
-
-
-for(let i=0;i<100;i++){
-
-
-let p=document.createElement("div");
-
-
-p.className="particle";
-
-
-p.style.left=
+star.style.left=
 Math.random()*100+"vw";
 
 
-p.style.animationDelay=
-Math.random()*5+"s";
+star.style.top=
+Math.random()*100+"vh";
+
+
+star.style.opacity=
+Math.random();
+
 
 
 document
-.querySelector(".particles")
-.appendChild(p);
+.querySelector(".stars")
+.appendChild(star);
 
 
 }
